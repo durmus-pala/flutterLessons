0c84f4a4bf664fdf5004b1c36d497020e14320c5
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-09-27 17:43:34 +0300

    go on lessons

--- flutter_first_lesson/lib/main.dart
+++ flutter_first_lesson/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(myApp());
 }
 
+// ignore: camel_case_types, use_key_in_widget_constructors
 class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,19 +16,28 @@ class myApp extends StatelessWidget {
         backgroundColor: Colors.yellow,
         appBar: AppBar(
           backgroundColor: Colors.teal,
-          title: Text('Başlık'),
+          title: const Text('Başlık'),
         ),
         // ignore: prefer_const_constructors
-        body: Text(
-          'Hello World',
+        body: Container(
+          width: 200,
+          height: 300,
+          color: Colors.purple,
+          alignment: Alignment.center,
           // ignore: prefer_const_constructors
-          style: TextStyle(
-              color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold),
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(10),
+          child: Text(
+            'Durmuş' * 10,
+            textAlign: TextAlign.center,
+            // ignore: prefer_const_constructors
+          ),
+          // ignore: prefer_const_constructors
+          // constraints: BoxConstraints(
+          //     minHeight: 100, minWidth: 100, maxHeight: 200, maxWidth: 200),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint('Tıklandı');
-          },
+          onPressed: () => {debugPrint('Tıklandı')},
           // ignore: prefer_const_constructors
           child: Icon(Icons.account_circle_rounded),
         ),
