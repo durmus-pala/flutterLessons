dff2ca693fdb9d68dd63df2ff7ebd9b605554fbd
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-09-28 15:52:40 +0300

    continue to lessons

--- flutter_first_lesson/lib/main.dart
+++ flutter_first_lesson/lib/main.dart
@@ -13,34 +13,69 @@ class myApp extends StatelessWidget {
       // ignore: deprecated_member_use
       theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.purple),
       home: Scaffold(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.teal,
           title: const Text('Başlık'),
         ),
         // ignore: prefer_const_constructors
         body: Container(
-          width: 200,
-          height: 300,
-          color: Colors.purple,
-          alignment: Alignment.center,
-          // ignore: prefer_const_constructors
-          margin: EdgeInsets.all(20),
-          padding: EdgeInsets.all(10),
-          child: Text(
-            'Durmuş' * 10,
-            textAlign: TextAlign.center,
-            // ignore: prefer_const_constructors
+          color: Colors.red.shade300,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.red,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.blue,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.orange,
+              )
+            ],
           ),
-          // ignore: prefer_const_constructors
-          // constraints: BoxConstraints(
-          //     minHeight: 100, minWidth: 100, maxHeight: 200, maxWidth: 200),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {debugPrint('Tıklandı')},
           // ignore: prefer_const_constructors
           child: Icon(Icons.account_circle_rounded),
         ),
+      ),
+    );
+  }
+
+  Widget containerDersleri() {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: FlutterLogo(
+          size: 128,
+        ),
+        decoration: BoxDecoration(
+            color: Colors.orange,
+            shape: BoxShape.rectangle,
+            border: Border.all(width: 4, color: Colors.purple),
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.green, offset: Offset(10, 20), blurRadius: 10),
+              BoxShadow(
+                  color: Colors.yellow, offset: Offset(0, -20), blurRadius: 20),
+            ]),
       ),
     );
   }
