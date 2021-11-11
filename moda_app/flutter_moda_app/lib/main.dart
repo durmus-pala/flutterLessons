53d1b39892f1b3fb14ca0b2289e353b3f1b1bc20
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-11-11 16:31:04 +0300

    complete moda_app

--- moda_app/flutter_moda_app/lib/main.dart
+++ moda_app/flutter_moda_app/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_moda_app/detay.dart';
 
-void main() => runApp(ModaApp());
+void main() => runApp(const ModaApp());
 
 class ModaApp extends StatelessWidget {
   const ModaApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AnaSayfa(),
     );
@@ -15,20 +16,75 @@ class ModaApp extends StatelessWidget {
 }
 
 class AnaSayfa extends StatefulWidget {
-  AnaSayfa({Key? key}) : super(key: key);
+  const AnaSayfa({Key? key}) : super(key: key);
 
   @override
   _AnaSayfaState createState() => _AnaSayfaState();
 }
 
-class _AnaSayfaState extends State<AnaSayfa> {
+class _AnaSayfaState extends State<AnaSayfa>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // ignore: todo
+    // TODO: implement dispose
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          indicatorColor: Colors.transparent,
+          controller: tabController,
+          tabs: const [
+            Tab(
+              icon: Icon(
+                Icons.more,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.play_arrow,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.navigation,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.supervised_user_circle,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text("Moda Uygulaması",
+        title: const Text("Moda Uygulaması",
             style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
@@ -36,45 +92,45 @@ class _AnaSayfaState extends State<AnaSayfa> {
         actions: [
           IconButton(
               onPressed: () {},
-              icon: Icon(Icons.camera_alt),
+              icon: const Icon(Icons.camera_alt),
               color: Colors.grey),
         ],
       ),
       body: ListView(
-        padding: EdgeInsets.only(top: 10),
+        padding: const EdgeInsets.only(top: 10),
         children: [
           // Üst taraftaki profil listesi
 
-          Container(
+          SizedBox(
             // color: Colors.blue.shade300,
             height: 140,
             width: double.infinity,
             child: ListView(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               scrollDirection: Axis.horizontal,
               children: [
                 listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
-                SizedBox(
+                const SizedBox(
                   width: 30,
                 ),
                 listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
-                SizedBox(
+                const SizedBox(
                   width: 30,
                 ),
                 listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
-                SizedBox(
+                const SizedBox(
                   width: 30,
                 ),
                 listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
-                SizedBox(
+                const SizedBox(
                   width: 30,
                 ),
                 listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
-                SizedBox(
+                const SizedBox(
                   width: 30,
                 ),
                 listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
-                SizedBox(
+                const SizedBox(
                   width: 30,
                 ),
               ],
@@ -86,11 +142,11 @@ class _AnaSayfaState extends State<AnaSayfa> {
             child: Material(
                 borderRadius: BorderRadius.circular(16),
                 elevation: 4,
-                color: Colors.blue.shade300,
+                //color: Colors.blue.shade300,
                 child: Container(
-                  height: 450,
+                  height: 500,
                   width: double.infinity,
-                  padding: EdgeInsets.all(16),
+                  padding: const EdgeInsets.all(16),
                   child: Column(
                     children: [
                       Row(
@@ -100,18 +156,18 @@ class _AnaSayfaState extends State<AnaSayfa> {
                             height: 50,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(20),
-                                image: DecorationImage(
+                                image: const DecorationImage(
                                     image: AssetImage('assets/model1.jpeg'),
                                     fit: BoxFit.cover)),
                           ),
-                          SizedBox(
+                          const SizedBox(
                             width: 10,
                           ),
-                          Container(
+                          SizedBox(
                             width: MediaQuery.of(context).size.width - 160,
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
+                              children: const [
                                 Text(
                                   "Daisy",
                                   style: TextStyle(
@@ -129,13 +185,198 @@ class _AnaSayfaState extends State<AnaSayfa> {
                               ],
                             ),
                           ),
-                          SizedBox(
+                          const SizedBox(
                             width: 10,
                           ),
-                          Icon(
+                          const Icon(
                             Icons.more_vert,
                             color: Colors.grey,
                             size: 22,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text(
+                        "This  official website features a ribbed knit zipper jacket that is"
+                        "modern and stylish. It looks very temparament and is recommend to friends",
+                        style: TextStyle(fontSize: 13, color: Colors.grey),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => Detay(
+                                      imgPath: 'assets/modelgrid1.jpeg')));
+                            },
+                            child: Hero(
+                              tag: 'assets/modelgrid1.jpeg',
+                              child: Container(
+                                height: 200,
+                                width:
+                                    (MediaQuery.of(context).size.width - 50) /
+                                        2,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: const DecorationImage(
+                                        image: AssetImage(
+                                            'assets/modelgrid1.jpeg'),
+                                        fit: BoxFit.cover)),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => Detay(
+                                          imgPath: 'assets/modelgrid2.jpeg')));
+                                },
+                                child: Hero(
+                                  tag: 'assets/modelgrid2.jpeg',
+                                  child: Container(
+                                    height: 95,
+                                    width: (MediaQuery.of(context).size.width -
+                                            100) /
+                                        2,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        image: const DecorationImage(
+                                            image: AssetImage(
+                                                'assets/modelgrid2.jpeg'),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => Detay(
+                                          imgPath: 'assets/modelgrid3.jpeg')));
+                                },
+                                child: Hero(
+                                  tag: 'assets/modelgrid3.jpeg',
+                                  child: Container(
+                                    height: 95,
+                                    width: (MediaQuery.of(context).size.width -
+                                            100) /
+                                        2,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        image: const DecorationImage(
+                                            image: AssetImage(
+                                                'assets/modelgrid3.jpeg'),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 25,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.brown.withOpacity(0.2)),
+                            child: const Center(
+                              child: Text(
+                                "#Louis vuitton",
+                                style: TextStyle(
+                                    fontSize: 10, color: Colors.brown),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            height: 25,
+                            width: 75,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.brown.withOpacity(0.2)),
+                            child: const Center(
+                              child: Text(
+                                "#Chloe",
+                                style: TextStyle(
+                                    fontSize: 10, color: Colors.brown),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Divider(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.reply,
+                            color: Colors.brown.withOpacity(0.2),
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(
+                            "1.7k",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(
+                            width: 25,
+                          ),
+                          Icon(
+                            Icons.comment,
+                            color: Colors.brown.withOpacity(0.2),
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(
+                            "325",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width - 235,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: const [
+                                Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                  size: 30,
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  "2.3k",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
                           )
                         ],
                       )
@@ -175,7 +416,7 @@ class _AnaSayfaState extends State<AnaSayfa> {
             ),
           ],
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
         Container(
@@ -183,7 +424,7 @@ class _AnaSayfaState extends State<AnaSayfa> {
           width: 75,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15), color: Colors.brown),
-          child: Center(
+          child: const Center(
             child: Text("Follow",
                 style: TextStyle(fontSize: 14, color: Colors.white)),
           ),
